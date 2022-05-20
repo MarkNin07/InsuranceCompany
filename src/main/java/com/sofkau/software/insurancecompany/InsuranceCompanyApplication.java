fcb488e050b816e84ef95ce9bf8aa17235f37890
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-20 11:51:54 -0500

    Project has been initialized. Sofka dependencies were installed

---
+++ src/main/java/com/sofkau/software/insurancecompany/InsuranceCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.software.insurancecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/software/insurancecompany/InsuranceCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.software.insurancecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
